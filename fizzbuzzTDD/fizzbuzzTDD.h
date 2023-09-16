843673ba7beb5bc01a44af8b34960c2c2a8c8883
MhmadRaziq <mhmd.abdraziq@gmail.com>
2023-09-16 12:50:28 +0300

    Initial Commit

---
+++ fizzbuzzTDD/fizzbuzzTDD.h
@@ -0,0 +1,18 @@
+//
+//  fizzbuzzTDD.h
+//  fizzbuzzTDD
+//
+//  Created by Mac on 16/09/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for fizzbuzzTDD.
+FOUNDATION_EXPORT double fizzbuzzTDDVersionNumber;
+
+//! Project version string for fizzbuzzTDD.
+FOUNDATION_EXPORT const unsigned char fizzbuzzTDDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <fizzbuzzTDD/PublicHeader.h>
+
+
